4a7f3117bb3521b87328ec5e42d308ce284d5acd
open_mailbox <games@open-mailbox.com>
2018-09-08 15:11:51 -0400

    Allow direction functions to operate using pointers to tiles.

--- pkg/puzzle/direction.go
+++ pkg/puzzle/direction.go
@@ -5,34 +5,34 @@ import (
 )
 
 // Describes how to traverse a puzzle in a particular direction
-type direction func(currentTile Tile) (nextTile Tile, e error)
+type direction func(puzzle Puzzle, currentTile *Tile) (nextTile *Tile, e error)
 
-func downToUp(puzzle Puzzle, currentTile Tile) (nextTile Tile, e error) {
+func downToUp(puzzle Puzzle, currentTile *Tile) (nextTile *Tile, e error) {
 	for _, tile := range puzzle.Tiles {
 		if tile.Y == currentTile.Y && tile.X == currentTile.X+1 {
-			return tile, nil
+			return &tile, nil
 		}
 	}
 
-	return Tile{}, errors.New("invalid tile location")
+	return &Tile{}, errors.New("invalid tile location")
 }
 
-func leftToRight(puzzle Puzzle, currentTile Tile) (nextTile Tile, e error) {
+func leftToRight(puzzle Puzzle, currentTile *Tile) (nextTile *Tile, e error) {
 	for _, tile := range puzzle.Tiles {
 		if tile.X == currentTile.X && tile.Y == currentTile.Y+1 {
-			return tile, nil
+			return &tile, nil
 		}
 	}
 
-	return Tile{}, errors.New("invalid tile location")
+	return &Tile{}, errors.New("invalid tile location")
 }
 
-func upToDown(puzzle Puzzle, currentTile Tile) (nextTile Tile, e error) {
+func upToDown(puzzle Puzzle, currentTile *Tile) (nextTile *Tile, e error) {
 	for _, tile := range puzzle.Tiles {
 		if tile.Y == currentTile.Y && tile.X == currentTile.X-1 {
-			return tile, nil
+			return &tile, nil
 		}
 	}
 
-	return Tile{}, errors.New("invalid tile location")
+	return &Tile{}, errors.New("invalid tile location")
 }
