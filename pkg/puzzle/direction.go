cf25704a1d0a2e5b880970dd8a46bfca58419923
open_mailbox <games@open-mailbox.com>
2018-09-08 14:27:20 -0400

    Add first-class functions for traversing a puzzle.

---
+++ pkg/puzzle/direction.go
@@ -0,0 +1,38 @@
+package puzzle
+
+import (
+	"errors"
+)
+
+// Describes how to traverse a puzzle in a particular direction
+type direction func(currentTile Tile) (nextTile Tile, e error)
+
+func downToUp(puzzle Puzzle, currentTile Tile) (nextTile Tile, e error) {
+	for _, tile := range puzzle.Tiles {
+		if tile.Y == currentTile.Y && tile.X == currentTile.X+1 {
+			return tile, nil
+		}
+	}
+
+	return Tile{}, errors.New("invalid tile location")
+}
+
+func leftToRight(puzzle Puzzle, currentTile Tile) (nextTile Tile, e error) {
+	for _, tile := range puzzle.Tiles {
+		if tile.X == currentTile.X && tile.Y == currentTile.Y+1 {
+			return tile, nil
+		}
+	}
+
+	return Tile{}, errors.New("invalid tile location")
+}
+
+func upToDown(puzzle Puzzle, currentTile Tile) (nextTile Tile, e error) {
+	for _, tile := range puzzle.Tiles {
+		if tile.Y == currentTile.Y && tile.X == currentTile.X-1 {
+			return tile, nil
+		}
+	}
+
+	return Tile{}, errors.New("invalid tile location")
+}
