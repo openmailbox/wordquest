6e93714ffd36adc02b7689d95951b84b464d9e78
open_mailbox <games@open-mailbox.com>
2018-09-12 23:35:39 -0400

    Set up initial websocket connection after page load.

--- internal/server.go
+++ internal/server.go
@@ -5,12 +5,23 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/websocket"
 	"github.com/openmailbox/wordquest/pkg/puzzle"
 )
 
+type game struct {
+	players []player
+}
+
+type player struct {
+	connection *websocket.Conn
+}
+
 const localAddress = ":8082"
 
+var currentGame game
 var currentPuzzle puzzle.Puzzle
+var upgrader = websocket.Upgrader{}
 
 func handlePuzzle(w http.ResponseWriter, r *http.Request) {
 	var tmpPuzzle struct {
@@ -24,22 +35,58 @@ func handlePuzzle(w http.ResponseWriter, r *http.Request) {
 	tmpPuzzle.Tiles = currentPuzzle.Tiles
 
 	json.NewEncoder(w).Encode(tmpPuzzle)
-	log.Printf("Completed %v %v\n", http.StatusOK, http.StatusText(http.StatusOK))
+}
+
+func handleUpdates(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		// TODO: Tell browser we can't upgrade so fallback to polling
+        log.Printf("Unable to establish websocket connection: %v\n", err)
+        return
+	}
+
+	currentGame.newPlayer(conn)
 }
 
 func logRequest(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%v %v from %v\n", r.Method, r.URL, r.RemoteAddr)
 		handler.ServeHTTP(w, r)
+		log.Printf("Completed %v %v\n", http.StatusOK, http.StatusText(http.StatusOK))
 	})
+}
+
+func (g *game) newPlayer(newConn *websocket.Conn) {
+	log.Printf("New connection from %v\n", newConn.RemoteAddr())
+
+	var newPlayer = player{connection: newConn}
+	currentGame.players = append(currentGame.players, newPlayer)
+
+	log.Printf("%v total players", len(currentGame.players))
+
+	// defer server.closeConnection(newPlayer)
+
+	go func() {
+		for {
+			_, message, err := newPlayer.connection.ReadMessage()
+			if err != nil {
+				log.Println(err)
+				break
+			}
+
+			log.Printf("%v\n", string(message))
+		}
+	}()
 }
 
 // StartServer - Starts the web server
 func StartServer(newPuzzle puzzle.Puzzle) {
 	currentPuzzle = newPuzzle
+	currentGame = game{}
 
 	log.Printf("Listening on %v...\n", localAddress)
 	http.Handle("/", http.FileServer(http.Dir("../../web/static")))
 	http.HandleFunc("/puzzle", handlePuzzle)
+	http.HandleFunc("/updates", handleUpdates)
 	log.Fatal(http.ListenAndServe(localAddress, logRequest(http.DefaultServeMux)))
 }
--- web/static/src/index.js
+++ web/static/src/index.js
@@ -1,19 +1,39 @@
 window.WordQuest = window.WordQuest || {};
 
 window.WordQuest.Puzzle = (function() {
-    var refresh = function() {
-        var oReq = new XMLHttpRequest();
-        oReq.addEventListener("load", puzzleCallback);
-        oReq.open("GET", "puzzle");
-        oReq.send();
+    var initialize = function() {
+        refresh_();
     };
 
-    var puzzleCallback = function(_evt) {
+    var refresh_ = function() {
+        var oReq = new XMLHttpRequest();
+        oReq.addEventListener("load", puzzleCallback_);
+        oReq.open("GET", "puzzle");
+        oReq.send();
+        subscribe_();
+    };
+
+    var puzzleCallback_ = function(_evt) {
         var data = JSON.parse(this.response);
         console.log(data);
     };
 
+    var subscribe_ = function() {
+        var socket = new WebSocket('ws://localhost:8082/updates');
+
+        socket.addEventListener('open', function(_evt) {
+            console.log("Established websocket connection!");
+            socket.send('Hello server!');
+        });
+
+        socket.addEventListener('message', function(evt) {
+            var data = JSON.parse(evt.data);
+
+            console.log(data);
+        });
+    }
+
     return {
-        refresh: refresh
+        initialize: initialize
     };
 })();
